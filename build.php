9aa1453e951fc1124e2f69c8de52210b373e90f1
Dave <crazeydave@users.noreply.github.com>
2026-04-04 04:10:29 +0100

    normalised name of top-level plugin file.

--- build.php
+++ build.php
@@ -176,7 +176,7 @@ class PluginBuilder
      */
     private function getVersionFromPlugin(): string
     {
-        $mainFile = $this->pluginDir . DIRECTORY_SEPARATOR . 'Sentinel.php';
+        $mainFile = $this->pluginDir . DIRECTORY_SEPARATOR . 'sentinel.php';
         if (file_exists($mainFile)) {
             $content = file_get_contents($mainFile);
             if (preg_match('/Version:\s*([0-9.]+\s*\w*)/', $content, $matches)) {
